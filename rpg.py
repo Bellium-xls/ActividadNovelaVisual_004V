14495d117a93d107b7112efcf3d77870c2da5a7d
Albedo-AB <di.aravenab@duocuc.cl>
2026-04-16 21:47:05 -0400

    tarea parte asesino

---
+++ rpg.py
@@ -0,0 +1,43 @@
+nombre = input("ingrese tu nombre aventurero:")
+
+print("...DESPERTASTE")
+print(f"{nombre}:¿donde estoy? ¿que es este lugar?")
+print("(vez a tu alrededor y solo ves oscuridad)")
+print("a lo lejos vez una ventana y estas en una especie de pieza")
+
+while True:
+    print(" 1.ver afuera de la ventana\n 2.buscar la puerta")
+
+    opciones = int(input("que haras ahora :"))
+    opcion_1 = 1
+    opcion_2 = 2
+
+    if opciones == 1:
+        print("ves afuera de la ventana.... solo vez niebla")
+        
+    else :
+        opciones == 2
+        print("encuentras una puerta y intentas abrirla")
+        break
+
+print("sales de una especie de celda")
+print(f"{nombre}: ¿como llegue hasta aca? porque hay un pasillo tan largo?")
+print(f"{nombre}: no veo el final")
+print("ves muchas puertas y cada una tiene un texto diferente")
+print("llegas a la primera puerta a mano derecha, tiene una fecha ")
+print(" 23/08/1960")
+print("te entra una jaqueca enorme")
+
+while True:
+    print("1.entrar en la puerta?\n2.seguir a la siguiente")
+    print(" (si sigues tu camino no habra regreso atras) ")
+    opciones = int(input("que haras ahora?:"))
+    opcion_1 = 1
+    opcion_2 = 2
+    if opcion_1 == 1:
+        print(" regresas a la fecha exacta.. 23/08/1960")
+    else:
+        opcion_2 == 2
+        print("llegas a la siguiente puerta a mano izquierda")
+        break
+    
