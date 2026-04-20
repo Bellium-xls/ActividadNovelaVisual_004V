7ba6faa73ad7c5bb7f56f9b35db5755049ca61e0
bellium-xls <sanscraftinghd@gmail.com>
2026-04-20 13:04:39 -0400

    AhoraSIFaltaLaParteDelASESINO

--- actividad_rpg.py
+++ actividad_rpg.py
@@ -5,7 +5,6 @@ def imprimir(texto, velocidad=0.03):
         time.sleep(velocidad)
     print()
 import sys
-
 imprimir("=== BIENVENIDO AL JUEGO, VE  COMIENZA TU AVENTURA ===")
 entrar = 1
 nombre_jugador = input("POR FAVOR INGRESE SU NOMBRE DE AVENTURERO: ")
@@ -90,20 +89,140 @@ if entrada_aljuego == 1:
                     print("No puedes huir en este combate, que lamentable.")
             imprimir("Al derrotar al jefe quedas exhausto del combate, decides tomar el botín que estaba a su lado y regresar al gremio para descansar.")
             print("==== AVENTURA FINALIZADA ====")
-
         elif clase == 2:
-            print("")
+            imprimir("Has decidido ir a la mazmorra para derrotar al jefe y empezar bien tu aventura.")
+            imprimir("Dentro de la mazmorra sientes un viento que te llena de energía, estas listo para cualquier combate.")
+            time.sleep(1)
+            imprimir(".")
+            time.sleep(1.5)
+            imprimir("..")
+            time.sleep(1.8)
+            imprimir("...")
+            imprimir("Aparece el jefe delante tuyo, tiene un aspecto grotesco",0.03)
+            bosshp = 500
+            bossatk = 20
+            while bosshp > 0 and hpcaballero > 0:
+                if bosshp <= 0:
+                    print("Combate finalizado.\nHaz derrotado al jefe.")
+                    break
+                elif hpcaballero <= 0:
+                    print("Haz sido derrotado por el jefe de la mazmorra, intentalo mejor la proxima vez.")
+                    sys.exit()
+                accion = int(input("\n1.Atacar\n2.Ver estadisticas\n3.Intentar huir. \n---> "))
+                if accion == 1:
+                    empalamiento = 3
+                    tajocelestial = 10
+                    ataquecaballero = int(input("\n1.|Empalamiento|\n2.|Tajo Celestial|\n---> "))
+                    if ataquecaballero == 1:
+                        bosshp = bosshp - (empalamiento * atkcaballero)
+                        print(f"Ataque efectivo, vida restante del enemigo : {bosshp}.")
+                    elif ataquecaballero == 2:
+                        bosshp = bosshp - (tajocelestial * atkcaballero)
+                        print(f"Azotas con fuertes vientos a tu enemigo, su vida restante : {bosshp}.")
+                    else:
+                        print("Haz pérdido un turno y el enemigo aprovecha para atacarte")
+                        hpcaballero -= bossatk
+                        print(f"Tu vida restante es : {hpcaballero}.")
+                elif accion == 2:
+                    print(f"Vida total del enemigo : {bosshp}.\nAtaque del enemigo : {bossatk}.")
+                elif accion == 3:
+                    print("No puedes huir en este combate, que lamentable.")
+            imprimir("Al derrotar al jefe quedas exhausto del combate, decides tomar el botín que estaba a su lado y regresar al gremio para descansar.")
+            print("==== AVENTURA FINALIZADA ====")
 
         elif clase == 3:
             print("")
 
     elif mision == 2:
         if clase == 1:
-            imprimir("Te armas de valor, sabes que probablemente no regreses con vida...",0.01, "Pero vale la pena intentarlo.")
-            
+            imprimir("Te armas de valor, sabes que probablemente no regreses con vida...")
+            time.sleep(2)
+            imprimir("Pero debes intentarlo.")
+            imprimir("En los pasillos del infierno vez como hay muchos cuerpos devorados con la carne destazada o incinerada, el olor es fuerte y no puedes aguantar taparte la nariz.")
+            imprimir("Sabes que Cerbero esta cerca tuyo, tu maná lo detecta cerca asi que te pones en guardia.")
+            print("Haz entrado en combate contra el perro del infierno, Cerbero.")
+            bosshp = 1500
+            bossatk = 15
+            while bosshp > 0 and hpmago > 0:
+                if bosshp <= 0:
+                    print("Combate finalizado.\nHaz derrotado a Cerbero.")
+                    break
+                elif hpmago <= 0:
+                    print("Haz sido devorado por Cerbero, intentalo mejor la proxima vez.")
+                    sys.exit()
+                accion = int(input("\n1.Atacar\n2.Ver estadisticas\n3.Curarse\n4.Intentar huir. \n---> "))
+                if accion == 1:
+                    rayodeluz = 5
+                    vendaval = 3
+                    pelluco = 10
+                    ataquesmago = int(input("\n1.|Rayo de luz|\n2.|Vendaval del sur|\n3.|Invocar a Pelluco|\n---> "))
+                    if ataquesmago == 1:
+                        bosshp = bosshp - (rayodeluz * apmago)
+                        manamago = manamago - 10
+                        print(f"Ataque efectivo, vida restante del enemigo : {bosshp}.")
+                    elif ataquesmago ==2:
+                        bosshp = bosshp - (vendaval * apmago)
+                        manamago -= 5
+                        print(f"Azotas con fuertes vientos a tu enemigo, su vida restante : {bosshp}.")
+                    elif ataquesmago == 3:
+                        print(f"¡Pelluco ha aparecido!\nEstá muy enojado por haberlo despertado de su sueño.")
+                        bosshp = bosshp - (pelluco *apmago)
+                        manamago -= 10
+                        print(f"Le ha causado fatales heridas al enemigo, su hp es de {bosshp}.")
+                    else:
+                        print("Haz pérdido un turno y el enemigo aprovecha para atacarte")
+                        hpmago -= bossatk
+                        print(f"Tu vida restante es : {hpmago}.")
+                elif accion == 2:
+                    print(f"Vida total del enemigo : {bosshp}.\nAtaque del enemigo : {bossatk}.")
+                elif accion == 3:
+                    if manamago > 0:
+                        hpmago += 15
+                        manamago -= 5
+                        print(f"Te haz curado 15 puntos vitales, tu HP restante : {hpmago}.\n Maná restante : {manamago}.")
+                    else:
+                        print("No tienes suficiente maná, no puedes curarte.")
+                elif accion == 4:
+                    print("No puedes huir en este combate, que lamentable.")
+            imprimir("Cerbero ha muerto en tus manos, puedes regresar al gremio para descansar y reclamar tu botín.")
+            print("==== AVENTURA FINALIZADA ====")
         elif clase == 2:
-            print("")
-        elif clase == 3:
+            imprimir("Como caballero, tienes confianza en ti y te preparas para bajar al infierno.")
+            time.sleep(2)
+            imprimir("Escuchas a Cerbero acercarse, sostienes tu espada con fuerza y te preparas para pelear.")
+            print("Haz entrado en combate contra el perro del infierno, Cerbero.")
+            bosshp = 1500
+            bossatk = 15
+            while bosshp > 0 and hpcaballero > 0:
+                if bosshp <= 0:
+                    print("Combate finalizado.\nHaz derrotado a Cerbero.")
+                    break
+                elif hpcaballero <= 0:
+                    print("Haz sido devorado por Cerbero, intentalo mejor la proxima vez.")
+                    sys.exit()
+                accion = int(input("\n1.Atacar\n2.Ver estadisticas\n3.Intentar huir. \n---> "))
+                if accion == 1:
+                    empalamiento = 3
+                    tajocelestial = 10
+                    ataquecaballero = int(input("\n1.|Empalamiento|\n2.|Tajo Celestial|\n---> "))
+                    if ataquecaballero == 1:
+                        bosshp = bosshp - (empalamiento * atkcaballero)
+                        print(f"Ataque efectivo, vida restante del enemigo : {bosshp}.")
+                    elif ataquecaballero == 2:
+                        bosshp = bosshp - (tajocelestial * atkcaballero)
+                        print(f"Azotas con fuertes vientos a tu enemigo, su vida restante : {bosshp}.")
+                    else:
+                        print("Haz pérdido un turno y el enemigo aprovecha para atacarte")
+                        hpcaballero -= bossatk
+                        print(f"Tu vida restante es : {hpcaballero}.")
+                elif accion == 2:
+                    print(f"Vida total del enemigo : {bosshp}.\nAtaque del enemigo : {bossatk}.")
+                elif accion == 3:
+                    print("No puedes huir en este combate, que lamentable.")
+            imprimir("Cerbero ha muerto, tu espada y escudo merecen un descanso despúes de esta hazaña, te vas de regreso al gremio.")
+            print("==== AVENTURA FINALIZADA ====")
+
+        elif clase == 3:    #Aqui va la actividad de asesino
             print("")
         
     else:
